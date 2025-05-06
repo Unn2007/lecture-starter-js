81e66c1babf4cbb1227bd27e193038090e05180c
unn2007 <unn2007@i.ua>
2025-05-06 18:12:24 +0300

    + implement getFighterDetails

--- src/javascript/services/fightersService.js
+++ src/javascript/services/fightersService.js
@@ -3,6 +3,8 @@ import callApi from '../helpers/apiHelper';
 class FighterService {
     #endpoint = 'fighters.json';
 
+    #endpointFighter = '../../../resources/api/details/fighter/';
+
     async getFighters() {
         try {
             const apiResult = await callApi(this.#endpoint);
@@ -15,6 +17,15 @@ class FighterService {
     async getFighterDetails(id) {
         // todo: implement this method
         // endpoint - `details/fighter/${id}.json`;
+
+        try {
+            const response = await fetch(`${this.#endpointFighter}${id}.json`);
+
+            return response.json();
+        } catch (error) {
+            console.error('Failed to fetch fighter details:', error);
+            throw error;
+        }
     }
 }
 
